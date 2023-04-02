547a49d022eba42f3c41a6bc75e090e53597009d
Karthik Solleti <karthik.solleti@hdworks.in>
2023-04-02 13:29:36 +0530

    build phase changes

--- CustomSliderFramework/CustomSliderFrameworkTests/CustomSliderFrameworkTests.swift
+++ CustomSliderFramework/CustomSliderFrameworkTests/CustomSliderFrameworkTests.swift
@@ -23,6 +23,7 @@ final class CustomSliderFrameworkTests: XCTestCase {
     func testHello() {
         CustomSlider().printHello()
     }
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
